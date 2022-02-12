a9f234053def353d8ad856218074d3c99dca07ae
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-02-12 23:49:24 +0300

    Use file backed data, add model and controller

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Admin Routes dispatcher
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes.routes);
 
 // User Routes dispatcher
 app.use(shopRoutes);
 
 // Catch all route
-app.use("/", (req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404");
-});
+// res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.use(errorController.get404);
 
 // const server = http.createServer(app);
 // server.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,22 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../helpers/path");
-const adminData = require("./admin");
+// const rootDir = require("../helpers/path");
+// const adminData = require("./admin");
+
+const productController = require("../controllers/products");
 
 const router = express.Router();
 
 // Root Route
-router.get("/", (req, res, next) => {
-  // with sendFile we are viewing the static html
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  //with render we are using the templating engine
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    docTitle: "Sercan's Shop",
-    path: "shopping",
-  });
-});
+router.get("/", productController.getProducts);
 
 module.exports = router;
